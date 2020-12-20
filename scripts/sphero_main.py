a85a155226b71c250af012b2562d543b26016944
Cédric GOUBARD <goubard.cedric@gmail.com>
2020-12-20 18:48:19 +0100

    Add topic with sensor data (not working yet)

--- scripts/sphero_main.py
+++ scripts/sphero_main.py
@@ -2,7 +2,10 @@
 import json
 
 import rospy
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Quaternion, Vector3
 from box import Box
+import numpy as np
 
 from sphero_mini.simple_moves import forward
 from sphero_mini.core import SpheroMini
@@ -13,7 +16,7 @@ def connect():
         cfg = Box(json.load(f))
 
     # Connect:
-    sphero = SpheroMini(cfg.MAC_ADDRESS, verbosity = 1)
+    sphero = SpheroMini(cfg.MAC_ADDRESS, verbosity = 4)
     # battery voltage
     sphero.getBatteryVoltage()
     print(f"Bettery voltage: {sphero.v_batt}v")
@@ -23,13 +26,85 @@ def connect():
     print(f"Firmware version: {'.'.join(str(x) for x in sphero.firmware_version)}")
     return sphero
 
+
 def disconnect(sphero):
     sphero.sleep()
     sphero.disconnect()
 
+
+def create_quaternion(roll, pitch, yaw):
+    q = Quaternion()
+    cy, sy = np.cos(yaw * 0.5), np.sin(yaw * 0.5)
+    cp, sp = np.cos(pitch * 0.5), np.sin(pitch * 0.5)
+    cr, sr = np.cos(roll * 0.5), np.sin(roll * 0.5)
+
+    q.w = cr * cp * cy + sr * sp * sy
+    q.x = sr * cp * cy - cr * sp * sy
+    q.y = cr * sp * cy + sr * cp * sy
+    q.z = cr * cp * sy - sr * sp * cy
+
+    return q
+
+
+def create_angular_veolocity_vector3(groll, gpitch, gyaw):
+    v = Vector3()
+    v.x = groll
+    v.y = gpitch
+    v.z = gyaw
+
+    return v
+
+
+def create_linear_acc_vector3(xacc, yacc, zacc):
+    v = Vector3()
+    v.x = xacc
+    v.y = yacc
+    v.z = zacc
+
+    return v
+
+
+def get_sensors_data(sphero):
+    return {
+        "roll": sphero.IMU_roll,
+        "pitch": sphero.IMU_pitch,
+        "yaw": sphero.IMU_yaw,
+        "groll": sphero.IMU_gyro_x,
+        "gpitch": sphero.IMU_gyro_y,
+        "xacc": sphero.IMU_acc_x,
+        "yacc": sphero.IMU_acc_y,
+        "zacc": sphero.IMU_acc_z
+    }
+    
+
+def publish_imu(pub, sphero):
+    i = Imu()
+    sensors_values = get_sensors_data(sphero)
+
+    i.header.stamp = rospy.Time.now()
+    i.orientation = create_quaternion(
+        roll=sensors_values["roll"],
+        pitch=sensors_values["pitch"],
+        yaw=sensors_values["yaw"]
+        )
+    i.angular_velocity = create_angular_veolocity_vector3(
+        groll=sensors_values["groll"],
+        gpitch=sensors_values["gpitch"],
+        gyaw=0  # We don't have the IMU_gyro_z
+    )
+    i.linear_acceleration = create_linear_acc_vector3(
+        xacc=sensors_values["xacc"],
+        yacc=sensors_values["yacc"],
+        zacc=sensors_values["zacc"]
+    )
+    pub.publish(i)
+
+
 def main(sphero):
     rospy.init_node('sphero', anonymous=True)   
     rate = rospy.Rate(10)  # 10 Hz
+
+    pub = rospy.Publisher("/imu", Imu, queue_size=5)
 
     sphero.configureSensorMask(
         IMU_yaw=True,
@@ -38,26 +113,20 @@ def main(sphero):
         IMU_acc_y=True,
         IMU_acc_z=True,
         IMU_acc_x=True,
+        IMU_gyro_x=True,
         IMU_gyro_y=True,
-        IMU_gyro_z=True 
+        #IMU_gyro_z=True 
         )
     sphero.configureSensorStream()
 
     while not rospy.is_shutdown():
-        print(sphero.__dict__)
-        print("IMU_pitch", sphero.IMU_pitch)
-        print("IMU_roll", sphero.IMU_roll)
-        print("IMU_yaw", sphero.IMU_yaw)
-        print("IMU_acc_y", sphero.IMU_acc_y)
-        print("IMU_acc_z", sphero.IMU_acc_z)
-        print("IMU_acc_x", sphero.IMU_acc_x)
-        print("IMU_gyro_y", sphero.IMU_gyro_y)
-        print("IMU_gyro_x", sphero.IMU_gyro_x)
+        publish_imu(pub, sphero)
         rate.sleep()
 
 
 if __name__ == "__main__":
     sphero = connect()
+
     try:
         main(sphero)
     except Exception as e: # rospy.ROSInterruptException
